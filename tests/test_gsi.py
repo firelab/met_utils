c973795f07956b5157de0e165e83ac1858a2da17
U-DS\bnordgren <bnordgren@LTHP24218GZ.usda.net>
2014-09-27 13:25:03 -0600

    added test to check that normalize was scaling things in the direction I expected. added comments.

--- tests/test_gsi.py
+++ tests/test_gsi.py
@@ -12,6 +12,10 @@ class TestNormalize(unittest.TestCase) :
         """a value about in the middle"""
         self.assertAlmostEqual(0.5, self.xform.scale(250))
         
+    def test_proportional(self) : 
+        """25% of the way from the lower bound"""
+        self.assertAlmostEqual(0.25, self.xform.scale(225))
+        
     def test_low(self) : 
         """a value off the low end of the scale"""
         self.assertAlmostEqual(0, self.xform.scale(100))
@@ -21,6 +25,7 @@ class TestNormalize(unittest.TestCase) :
         self.assertAlmostEqual(1, self.xform.scale(5000))
         
     def test_array(self) : 
+        """give it an array instead of a scalar"""
         x = np.array( [200, 250, 300] )
         x_prime = self.xform.scale(x)
         self.assertEqual(len(x_prime), 3)
@@ -36,35 +41,45 @@ class TestGSI(unittest.TestCase) :
         self.xf_photo= self.gsi.xf_photo
         
     def test_photoperiod_low(self) : 
+        """a value off the low end of the scale"""
         self.assertLess(self.xf_photo.scale(9*u.hour) - 0, 1e-5)
         
     def test_photoperiod_middle(self) : 
+        """a value about in the middle"""
         self.assertLess(self.xf_photo.scale(10.5*u.hour) - 0.5, 1e-5)
         
     def test_photoperiod_high(self) : 
+        """a value off the top end of the scale"""
         self.assertLess(self.xf_photo.scale(12*u.hour) - 1.0, 1e-5)
         
     def test_vpd_low(self) : 
+        """a value off the low end of the scale"""
         self.assertLess(self.xf_vpd.scale(200*u.Pa) - 0, 1e-5)
         
     def test_vpd_middle(self) :
+        """a value about in the middle"""
         midrange =  (gsi.VPD_MIN + gsi.VPD_MAX)/2
         self.assertLess(self.xf_vpd.scale(midrange)- 0.5, 1e-5)
         
     def test_vpd_high(self) : 
+        """a value off the top end of the scale"""
         self.assertLess(self.xf_vpd.scale(10000*u.Pa) - 1., 1e-5)
         
     def test_tmin_low(self) : 
+        """a value off the low end of the scale"""
         self.assertLess(self.xf_tmin.scale(-20.*u.deg_C) - 0, 1e-5)
         
     def test_tmin_middle(self) : 
+        """a value about in the middle"""
         midrange = (gsi.TMIN_MIN + gsi.TMIN_MAX) / 2.
         self.assertLess(self.xf_tmin.scale(midrange) - 0.5, 1e-5)
         
     def test_tmin_high(self) :
+        """a value off the top end of the scale"""
         self.assertLess(self.xf_tmin.scale(40.*u.deg_C) - 1., 1e-5)
         
     def test_gsi_sglval(self):
+        """test entire gsi calculation with scalar inputs"""
         tmin = (gsi.TMIN_MIN + gsi.TMIN_MAX)/2.
         photo = (gsi.PHOTO_MIN + gsi.PHOTO_MAX) / 2.
         vpd = (gsi.VPD_MIN + gsi.VPD_MAX) / 2.
@@ -72,6 +87,7 @@ class TestGSI(unittest.TestCase) :
         self.assertLess(self.gsi.evaluate(tmin, vpd, photo) - (0.5**3), 1e-5)
         
     def test_gsi_array(self): 
+        """test entire gsi calculation with array inputs"""
         frac = np.arange(0.0, 1.0, 0.2)
         tmin = frac * (gsi.TMIN_MAX-gsi.TMIN_MIN) + gsi.TMIN_MIN
         photo = frac * (gsi.PHOTO_MAX-gsi.PHOTO_MIN) + gsi.PHOTO_MIN
