911512ee020bdba2fd0a123ec91bd703cb986617
U-DS\bnordgren <bnordgren@LTHP24218GZ.usda.net>
2014-09-27 13:16:37 -0600

    Wrote GSI calculator and tests

---
+++ gsi.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+from astropy import units as u
+
+# these constants affect the scaling of various met variables.
+# Values are taken from [1].
+#
+# [1] Jolly, William M., Ramakrishna Nemani, and Steven W. Running. 2005. 
+#     “A Generalized, Bioclimatic Index to Predict Foliar Phenology in 
+#     Response to Climate.” Global Change Biology 11 (4): 619–32. 
+#     doi:10.1111/j.1365-2486.2005.00930.x.
+TMIN_MIN = -2. * u.deg_C
+TMIN_MAX =  5. * u.deg_C
+VPD_MIN  = 900. * u.Pa
+VPD_MAX  = 4100. * u.Pa
+PHOTO_MIN = 10. * u.hour
+PHOTO_MAX = 11. * u.hour
+
+
+class Normalize (object) : 
+    """Class scales values from 0 to 1 given a min and a max"""
+    def __init__(self, minval, maxval) : 
+        self._minval = minval
+        self._maxval = maxval
+        
+    def scale(self, x) : 
+        scaled_x = (x - self._minval) / (self._maxval - self._minval)
+        
+        # if x is an ndarry, do the clipping in place
+        if (isinstance(x, np.ndarray)) : 
+            scaled_x = np.clip(scaled_x, 0, 1, out=scaled_x)
+        else : 
+            scaled_x = np.clip(scaled_x, 0, 1)
+            
+        return scaled_x
+        
+class GSI (object) : 
+    """Implements growing season index calculations.
+    
+    For more information see [1]
+    
+    [1] Jolly, William M., Ramakrishna Nemani, and Steven W. Running. 2005. 
+        “A Generalized, Bioclimatic Index to Predict Foliar Phenology in Response 
+        to Climate.” Global Change Biology 11 (4): 619–32. 
+        doi:10.1111/j.1365-2486.2005.00930.x.
+    """
+    def __init__(self) :
+        self.xf_tmin = Normalize(TMIN_MIN, TMIN_MAX)
+        self.xf_vpd  = Normalize(VPD_MIN, VPD_MAX)
+        self.xf_photo= Normalize(PHOTO_MIN, PHOTO_MAX)
+        
+    def evaluate(self, tmin, vpd, photo) : 
+        """calculate the growing season index
+        
+        input variables must be defined using the correct units or errors
+        will result.
+        
+        Parameters
+        ----------
+        tmin : array : deg_C
+            daily minimum temperature
+        vpd  : array : Pa
+            vapor pressure deficit
+        photo : array : hours
+            photoperiod: time from sunrise to sunset
+            
+        Returns
+        -------
+        gsi : array : dimensionless
+            growing season index
+        """
+        tmin_i = self.xf_tmin.scale(tmin)
+        vpd_i  = 1 - self.xf_vpd.scale(vpd)
+        photo_i= self.xf_photo.scale(photo)
+        return tmin_i * vpd_i * photo_i
---
+++ tests/test_gsi.py
@@ -0,0 +1,83 @@
+import unittest
+from astropy import units as u
+import numpy as np
+import gsi
+
+
+class TestNormalize(unittest.TestCase) : 
+    def setUp(self) : 
+        self.xform = gsi.Normalize(200.,300.)
+        
+    def test_middle(self) : 
+        """a value about in the middle"""
+        self.assertAlmostEqual(0.5, self.xform.scale(250))
+        
+    def test_low(self) : 
+        """a value off the low end of the scale"""
+        self.assertAlmostEqual(0, self.xform.scale(100))
+        
+    def test_high(self) : 
+        """a value off the top end of the scale"""
+        self.assertAlmostEqual(1, self.xform.scale(5000))
+        
+    def test_array(self) : 
+        x = np.array( [200, 250, 300] )
+        x_prime = self.xform.scale(x)
+        self.assertEqual(len(x_prime), 3)
+        self.assertAlmostEqual(0, x_prime[0])
+        self.assertAlmostEqual(0.5, x_prime[1])
+        self.assertAlmostEqual(1, x_prime[2])
+        
+class TestGSI(unittest.TestCase) : 
+    def setUp(self) :
+        self.gsi = gsi.GSI()
+        self.xf_tmin = self.gsi.xf_tmin
+        self.xf_vpd  = self.gsi.xf_vpd
+        self.xf_photo= self.gsi.xf_photo
+        
+    def test_photoperiod_low(self) : 
+        self.assertLess(self.xf_photo.scale(9*u.hour) - 0, 1e-5)
+        
+    def test_photoperiod_middle(self) : 
+        self.assertLess(self.xf_photo.scale(10.5*u.hour) - 0.5, 1e-5)
+        
+    def test_photoperiod_high(self) : 
+        self.assertLess(self.xf_photo.scale(12*u.hour) - 1.0, 1e-5)
+        
+    def test_vpd_low(self) : 
+        self.assertLess(self.xf_vpd.scale(200*u.Pa) - 0, 1e-5)
+        
+    def test_vpd_middle(self) :
+        midrange =  (gsi.VPD_MIN + gsi.VPD_MAX)/2
+        self.assertLess(self.xf_vpd.scale(midrange)- 0.5, 1e-5)
+        
+    def test_vpd_high(self) : 
+        self.assertLess(self.xf_vpd.scale(10000*u.Pa) - 1., 1e-5)
+        
+    def test_tmin_low(self) : 
+        self.assertLess(self.xf_tmin.scale(-20.*u.deg_C) - 0, 1e-5)
+        
+    def test_tmin_middle(self) : 
+        midrange = (gsi.TMIN_MIN + gsi.TMIN_MAX) / 2.
+        self.assertLess(self.xf_tmin.scale(midrange) - 0.5, 1e-5)
+        
+    def test_tmin_high(self) :
+        self.assertLess(self.xf_tmin.scale(40.*u.deg_C) - 1., 1e-5)
+        
+    def test_gsi_sglval(self):
+        tmin = (gsi.TMIN_MIN + gsi.TMIN_MAX)/2.
+        photo = (gsi.PHOTO_MIN + gsi.PHOTO_MAX) / 2.
+        vpd = (gsi.VPD_MIN + gsi.VPD_MAX) / 2.
+        
+        self.assertLess(self.gsi.evaluate(tmin, vpd, photo) - (0.5**3), 1e-5)
+        
+    def test_gsi_array(self): 
+        frac = np.arange(0.0, 1.0, 0.2)
+        tmin = frac * (gsi.TMIN_MAX-gsi.TMIN_MIN) + gsi.TMIN_MIN
+        photo = frac * (gsi.PHOTO_MAX-gsi.PHOTO_MIN) + gsi.PHOTO_MIN
+        vpd = (1-frac) * (gsi.VPD_MAX-gsi.VPD_MIN) + gsi.VPD_MIN
+        
+        i_gsi = self.gsi.evaluate(tmin,vpd, photo)
+        
+        for i in range(len(frac)) :
+            self.assertLess(i_gsi[i] - (frac[i]**3), 1e-5)
