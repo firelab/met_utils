fc88fedecc2d7a39cc111aa75ddb2f3c7711d417
U-DS\bnordgren <bnordgren@LTHP24218GZ.usda.net>
2014-10-17 17:20:17 -0600

    Removed the dependency on three ra/dec locations on three consecutive days.

---
+++ sun.py
@@ -0,0 +1,414 @@
+"""
+Simple, approximate calculations for solar position and characteristics.
+
+This module implements calculations pertaining to the calculation of 
+solar coordinates given in [1]. These calculations predict the geometric 
+position of the sun to 0.01 degrees by assuming a purely elliptical Earth
+orbit. 
+
+[1] Meeus, Jean. Astronomical Algorithms. 2nd edition. Richmond, Va: 
+    Willmann-Bell, 1998.
+"""
+import astropy.units as u
+import astropy.time as time
+import astropy.coordinates as c
+import datetime as dt
+import numpy as np
+
+julian_century = u.def_unit("julian_century", 36525*u.day)
+j2000_epoch = time.Time(val='2000:001:12:00', scale='tt', format='yday')
+
+
+def jc(d) : 
+    """Converts a standard date to a time delta from J2000 epoch
+    
+    Most, if not all of the equations in this file are empirical fits 
+    using delta time from an epoch."""
+    return (d-j2000_epoch).to(julian_century)
+
+def day(d, scale) : 
+    """converts a full timestamp to a date
+    """
+    date = d.datetime.date()
+    d1 = dt.datetime(date.year,date.month,date.day)
+    return time.Time(d1,scale=scale)
+
+def geometric_mean_longitude(t) : 
+    """geometric mean longitude of the sun referred to mean equinox of the date
+    
+    Equation 25.2, p 163.
+    
+    Parameters
+    ----------
+    t : array : Julian centuries (36525 days)
+    
+    Returns
+    -------
+    L0 : array: deg
+    """
+    return 280.46646*u.deg + (36000.76983*u.deg/julian_century)*t + (0.0003032*u.deg/(julian_century**2))*(t**2)
+    
+def mean_anomaly(t) : 
+    """Mean anomaly of the sun (earth)
+    
+    Equation 25.3, p. 163, different from equation on p. 144
+    
+    Parameters
+    ----------
+    t : array : Julian centuries
+    
+    Returns
+    -------
+    M : array : deg
+    """
+    return (357.52911*u.deg + 
+            (35999.05029*u.deg/julian_century)*t +
+            (0.0001537*u.deg/(julian_century**2))*(t**2))
+            
+def equation_of_center(t, m) :
+    """Sun equation of center
+     
+    Unnumbered equation, top of page 164.
+      
+    Parameters
+    ----------
+    t : array : julian centuries
+    
+    m : array : deg
+        solar/earth mean anomaly
+        
+    Returns
+    -------
+    c : array : deg
+    """
+    return ( (1.914602*u.deg - (0.004817*u.deg/julian_century)*t - (0.000014*u.deg/(julian_century**2))*t**2)*np.sin(m)
+             +(0.019993*u.deg - (0.000101*u.deg/julian_century)*t)*np.sin(2*m)
+             +(0.000289*u.deg*np.sin(3*m)))
+
+def mean_ecliptic_obliquity(t) : 
+    """calculates the mean obliquity of the ecliptic at the specified time
+    
+    Equation 22.2, pg. 147. The accuracy of this equation is estimated to 
+    be 0.01 arcseconds for 1000 years to either side of the epoch in 2000 A.D.
+    
+    Parameters
+    ----------
+    t : array : julian centuries
+    
+    Returns
+    -------
+    epsilon0  : array : deg
+    """
+    return ( c.Angle( (23,26,21.448), unit=u.deg) 
+            - (46.8150 * u.arcsec / julian_century)*t
+            - (0.00059 * u.arcsec / (julian_century**2)*(t**2))
+            + (0.001813 * u.arcsec / (julian_century**3)*(t**3)))
+
+
+def lon_moon_ascending_node(t) :
+    """Longitude of the ascending node of the moon's mean orbit on the ecliptic
+    
+    Unnumbered equation, pg. 144.
+    """
+    return ( 125.04452*u.deg - (1934.136261*u.deg/julian_century)*t +
+              (0.0020708 * u.deg/(julian_century**2))*(t**2) + 
+              (t**3)/(450000 * (julian_century**3)/u.deg) )
+
+
+def body_altitude(dec, obs_lat, hour_angle) : 
+    """Calculates the altitude (elevation) of a body given the sky coords and hour angle
+    
+    Equation 13.6, p 93
+    """
+    sin_alt = np.sin(obs_lat)*np.sin(dec) + np.cos(obs_lat)*np.cos(dec)*np.cos(hour_angle)
+    return np.arcsin(sin_alt)
+    
+def fix_day(time, unit) : 
+    if time < 0*unit : 
+        time += 1*unit
+    elif time > 1*unit : 
+        time -= 1*unit
+    return time
+    
+
+class Ecliptic(object) : 
+    def __init__(self, time) : 
+        self.time = time 
+        self.jc_time = jc(time)
+        self._mean_obliquity = None
+        self._moon_asc_lon = None
+        self._nutation_obliquity = None
+        self._true_obliquity = None
+        
+    def get_mean_obliquity(self) : 
+        if self._mean_obliquity == None : 
+            self._mean_obliquity = mean_ecliptic_obliquity(self.jc_time)
+        return self._mean_obliquity
+        
+    def get_moon_asc_lon(self) : 
+        if self._moon_asc_lon == None : 
+            self._moon_asc_lon = lon_moon_ascending_node(self.jc_time)
+        return self._moon_asc_lon
+        
+    def get_nutation_obliquity(self) :
+        """calculate the nutation in obliquity
+        
+        Equation 25.8, p 165.
+        This is a very course approximation, which is essentially the first term
+        in a somewhat finer approximation toward the bottom
+        of page 144. 
+        """
+        if self._nutation_obliquity == None : 
+            omega = self.get_moon_asc_lon()
+            self._nutation_obliquity = 9.2*u.arcsec * np.cos(omega)
+        return self._nutation_obliquity
+        
+    def get_true_obliquity(self) : 
+        """calculate the true obliquity
+        
+        This is the mean obliquity + the nutation in obliquity.
+        """
+        if self._true_obliquity == None :
+            delta_eps = self.get_nutation_obliquity()
+            eps0 = self.get_mean_obliquity()
+            self._true_obliquity = eps0 + delta_eps
+        return self._true_obliquity
+                  
+class SunPosition(object) : 
+    """Orchestrates the various positional calculations in order to minimize redundancy."""
+    def __init__(self, time) : 
+        self.time = time
+        self.jc_time = jc(time)
+        self.disk_radius = 16*u.arcmin
+        self._mean_longitude = None
+        self._mean_anomaly = None
+        self._eoc = None
+        self._true_longitude = None
+        self._true_anomaly = None
+        self._apparent_longitude = None
+        self._true_pos = None
+        self._apparent_pos = None
+        self._ecliptic = None
+    
+    def get_mean_lon(self) : 
+        if self._mean_longitude == None : 
+            self._mean_longitude = geometric_mean_longitude(self.jc_time)
+        return self._mean_longitude
+    
+    def get_mean_anomaly(self) : 
+        if self._mean_anomaly == None : 
+            self._mean_anomaly = mean_anomaly(self.jc_time)
+        return self._mean_anomaly
+        
+    def get_eoc(self) : 
+        if self._eoc == None : 
+            m = self.get_mean_anomaly()
+            self._eoc = equation_of_center(self.jc_time, m)
+        return self._eoc
+    
+    def get_true_lon(self):  
+        """calculate true longitude
+        
+        Unnumbered equation, p. 164
+        """
+        if self._true_longitude == None : 
+            l0 = self.get_mean_lon()
+            eoc = self.get_eoc()
+            self._true_longitude = c.Angle(l0 + eoc).wrap_at(360*u.deg)
+        return self._true_longitude
+        
+    def get_true_anomaly(self) : 
+        """calculate true anomaly
+        
+        Unnumbered equation, p. 164
+        """
+        if self._true_anomaly == None : 
+            m = self.get_mean_anomaly()
+            eoc = self.get_eoc()
+            self._true_anomaly = m + eoc
+        return self._true_anomaly
+        
+    def get_apparent_lon(self) : 
+        """calculate the apparent solar longitude
+        
+        Unnumbered equation, p. 164
+        """
+        if self._apparent_longitude == None : 
+            omega = 125.04*u.deg - (1934.136*u.deg/julian_century)*self.jc_time
+            lon = self.get_true_lon()
+            self._apparent_longitude = c.Angle(lon - 0.00569*u.deg - (0.00478*u.deg)*np.sin(omega)).wrap_at(360*u.deg)
+        return self._apparent_longitude
+        
+    def get_ecliptic(self) : 
+        if self._ecliptic == None : 
+            self._ecliptic = Ecliptic(self.time)
+        return self._ecliptic
+        
+    def _get_position(self, epsilon, lon) : 
+        """calculates the sun position given the longitude
+        
+        Equations 25.6 and 25.7 on p. 165. If true position is desired, 
+        pass the true longitude, if apparent position, pass apparent longitude.
+        
+        Parameters
+        ----------
+        epsilon : array : deg
+            mean or true obliquity of the elliptic
+        lon : array : deg
+            longitude, either apparent or true
+        
+        Returns
+        -------
+        pos : array : deg
+            SkyCoord object populated with the ra and dec of the sun's position
+        """
+        sinlon = np.sin(lon)
+        ra = np.arctan2(np.cos(epsilon)*sinlon, np.cos(lon))
+        dec = np.arcsin(np.sin(epsilon)*sinlon)
+        return c.SkyCoord(ra=ra, dec=dec, obstime=self.time)
+        
+    def get_true_position(self) : 
+        if self._true_pos == None : 
+            epsilon = self.get_ecliptic().get_mean_obliquity()
+            self._true_pos = self._get_position(epsilon, self.get_true_lon())
+        return self._true_pos
+        
+    def get_apparent_position(self) : 
+        if self._apparent_pos == None : 
+            epsilon = self.get_ecliptic().get_true_obliquity()
+            self._apparent_pos = self._get_position(epsilon, self.get_apparent_lon())
+        return self._apparent_pos
+        
+class Uptime(object) : 
+    def __init__(self, body_class, time, obs_location, refraction=(34*u.arcmin)) : 
+        self.obs_location = obs_location
+        self.refraction = refraction
+        self.midnight_utc = day(time, 'utc')
+        self.midnight_tt  = day(time, 'tt')
+        self.body_class = body_class
+        self.body = body_class(self.midnight_utc)
+        self.h0 = - (refraction + self.body.disk_radius)
+        self.sidereal_greenwich = self.midnight_utc.sidereal_time('apparent','greenwich')
+        self._approx_m = None
+        self._correction_m = None
+        
+    def approximate(self) : 
+        """performs the approximate calculation of sunrise/sunset times
+        
+        Equation 15.1 and 15.2, p. 102
+        These calculations are good to approximately +-0.01 days (+-14.4 minutes)
+        """
+        if self._approx_m != None : 
+            return self._approx_m
+            
+            
+        pos = self.body.get_apparent_position()
+        dec = pos.dec
+        ra  = pos.ra
+        lat = self.obs_location.latitude
+        lon = self.obs_location.longitude
+        
+        # eq 15.1
+        cos_H0 = ((np.sin(self.h0) - (np.sin(lat)*np.sin(dec)))/
+                  (np.cos(lat)*np.cos(dec)))
+        np.clip(cos_H0, -1, 1)
+        H0 = np.arccos(cos_H0)
+        
+        # eq 15.2
+        # transit time, fractional day
+        # modified from eq. 15.2 because 15.2 expects longitude to be 
+        # positive "west" instead of positive "east".
+        m0 = (ra - lon - self.sidereal_greenwich)/(360*u.deg/u.sday)
+        # delta time, transit to sunrise/set
+        delta_t = H0/(360*u.deg/u.sday)
+        m1 = m0 - delta_t
+        m2 = m0 + delta_t
+        self._approx_m = u.Quantity([fix_day(m0, u.sday),
+                                     fix_day(m1, u.sday),
+                                     fix_day(m2, u.sday)])
+        return self._approx_m
+    
+    def _correct_rise_set(self, h, dec, lat, hour_angle):
+        """implements correction to rise/set time offsets
+        
+        Unnumbered equation, p. 103
+        """
+        delta_m = (h-self.h0) / ((360*u.deg/u.day)*np.cos(dec)*np.cos(lat)*np.sin(hour_angle))
+        return delta_m
+    
+    def correction(self) :
+        """Calculates a correction to transit/rise/set times
+        
+        Implements unnumbered equations on p. 103, defining a correction
+        to m. These should be relatively small, on the order of +-0.01 days
+        (+-14 minutes)
+        """ 
+        if self._correction_m != None : 
+            return self._correction_m
+            
+        # calculate sidereal time at each event
+        # scale m such that it represents the same fraction of a solar day that 
+        # it represented in a sidereal day. Because a solar day is longer,
+        # the fraction of the day will also be longer.
+        # also change units.
+        #m_solar_day = (self.approximate()*1.0027379093604878) * (u.day/u.sday)
+        m_solar_day = c.Angle(self.approximate()*360.985647 * u.deg/u.sday)
+        
+        # sidereal times at greenwich of the events (transit/rise/set)
+        # have to convert to hour, or else it won't add to an hourangle
+        #sidereal_t = self.sidereal_greenwich + c.Angle(m_solar_day.to(u.hour))
+        sidereal_t = self.sidereal_greenwich + m_solar_day
+        sidereal_t.wrap_at(360*u.deg, inplace=True)
+        
+        # times of the events
+        t_events = self.midnight_utc + (m_solar_day/(360*u.deg/u.sday))
+        
+        # calculate apparent positions at the time of the events
+        body = self.body_class(t_events.tt)
+        pos = body.get_apparent_position()
+        
+        # calculate local hour angle of the body
+        # changed sign on longitude because formula in book expects
+        # longitudes to be numbered positive westward.
+        H = sidereal_t + self.obs_location.longitude - pos.ra
+        
+        # calculate altitude of the body for rise/set events
+        alt = body_altitude(pos[1:].dec,self.obs_location.latitude, H[1:]) 
+        
+        # calculate the corrections
+        dm0 = - (H[0]/(360*u.deg/u.day))
+        dm1 = self._correct_rise_set(alt[0], pos[1].dec,self.obs_location.latitude,H[1])
+        dm2 = self._correct_rise_set(alt[1], pos[2].dec,self.obs_location.latitude,H[2])
+        
+        self._correction_m = u.Quantity([dm0,dm1,dm2]).to(u.day)
+        return self._correction_m
+        
+    def accurate(self) : 
+        """produces a more accurate set of times than approximate"""
+        return self.approximate() + self.correction()
+        
+    def approximate_times(self): 
+        """returns actual timestamps for the events instead of offsets within the day"""
+        return self.midnight_utc + self.approximate()
+        
+    def accurate_times(self) : 
+        """returns actual timestamps for the events instead of offsets within the day"""
+        return self.midnight_utc + self.accurate()
+        
+    def approximate_daylength(self): 
+        times = self.approximate()
+        
+        daylength = times[2] - times[1]
+        # note: np.choose strips off units.
+        # we're messing with the daylength because if the rise or set time was on
+        # the previous/next UTC day, then 1 day was added/subtracted. The effect
+        # will be that the sun sets before it rises, and hence a blind subtraction
+        # yields the negative of the nightlength. Add one to get daylength
+        daylength = np.choose(daylength<0*u.sday, [daylength, daylength+1*u.sday])
+        return daylength * u.sday
+        
+    def accurate_daylength(self) : 
+        times = self.accurate()
+        daylength = times[2] - times[1]
+        daylength = (daylength<0*u.sday, [daylength, daylength+1*u.sday])
+        return daylength
---
+++ tests/test_sun.py
@@ -0,0 +1,192 @@
+"""Tests code related to predicting sun position from [1]
+
+[1] Meeus, Jean. 1998. Astronomical Algorithms. 2nd edition. Richmond, Va: Willmann-Bell.
+"""
+
+import unittest
+import sun
+import astropy.units as u
+import astropy.coordinates as c
+import astropy.time as t
+import numpy as np
+
+class TestEcliptic(unittest.TestCase) : 
+    """Exercises the Ecliptic class using test case data from [1]
+    
+    In each test, the computed values are tested to match within 1/2 of 
+    the least significant digit in the reported value.
+    """
+    def setUp(self) : 
+        self.td = t.Time("1987-04-10", scale='tt')
+        
+    def test_mean_obliquity(self) : 
+        """Test mean olbiquity computation
+        
+        Test data from example 22a on p 148"""
+        e = sun.Ecliptic(self.td)
+        me = e.get_mean_obliquity()
+        self.assertLess(np.abs(me - c.Angle( (23,26,27.407), unit=u.deg)), 0.0005*u.arcsec)
+        
+    def test_true_obliquity(self) : 
+        """Test true olbiquity computation (hence nutation as well)
+        
+        Test data from example 25a on p 165, since nutation is implemented using
+        approximation in Equation 25.8"""
+        e = sun.Ecliptic(t.Time('1992-10-13', scale='tt'))
+        te = e.get_true_obliquity()
+        self.assertLess(np.abs(te - 23.43999*u.deg), 0.000005*u.deg)
+        
+    def test_moon_asc_lon(self) : 
+        """Test longitude of the ascending node of the Moon's mean orbit on the ecliptic
+        
+        Test data from example 22a on p 148"""
+        e = sun.Ecliptic(self.td)
+        moon_lon = e.get_moon_asc_lon()
+        self.assertLess(np.abs(moon_lon - c.Angle(371.2531*u.deg)), 0.00005*u.deg)
+        
+    def test_nutation_obliquity(self): 
+        """Test computation of nutation in obliquity
+        
+        Test data from example 25.a on p. 165, due to the fact that the class
+        implements the approximation in equation 25.8, p. 165
+        """
+        td = t.Time('1992-10-13', scale='tt')
+        e = sun.Ecliptic(td)
+        delta_eps = e.get_nutation_obliquity()
+        delta_eps_given = 23.43999 * u.deg - c.Angle( (23, 26, 24.83), unit=u.deg)
+        self.assertLess(np.abs(delta_eps - delta_eps_given), 0.000005*u.deg)
+
+class TestSunPosition(unittest.TestCase)  : 
+    def setUp(self) :
+        """Data here come from example 25a, p165""" 
+        self.t = t.Time('1992-10-13', scale='tt')
+        self.pos = sun.SunPosition(self.t)
+        
+    def test_julian_century(self) : 
+        """basic test of conversion to julian centuries"""
+        jc = sun.jc(self.t)
+        self.assertLess(np.abs(jc - (-0.072183436 * sun.julian_century)), 0.5e-9 * sun.julian_century)
+        
+    def test_geometric_mean_lon(self) :
+        """tests geometric mean lon calculation"""
+        L0 = self.pos.get_mean_lon() 
+        self.assertLess(np.abs(L0 - c.Angle(-2318.19280*u.deg)), 0.5e-5*u.deg)
+        
+    def test_mean_anomaly(self) : 
+        """tests the geometric mean anomaly calculation"""
+        M = self.pos.get_mean_anomaly()
+        self.assertLess(np.abs(M - c.Angle(-2241.00603*u.deg)), 0.5e-5*u.deg)
+        
+    def test_eoc(self) : 
+        """tests the equation of center calculations"""
+        eoc = self.pos.get_eoc() 
+        self.assertLess(np.abs(eoc- c.Angle(-1.89732*u.deg)), 0.5e-5*u.deg)
+        
+    def test_true_lon(self) : 
+        """tests calculation of sun's true longitude"""
+        lon = self.pos.get_true_lon()
+        self.assertLess(np.abs(lon - c.Angle(199.90988*u.deg)), 1.0e-5*u.deg)
+        
+    def test_apparent_lon(self) : 
+        """tests calculation of sun's apparent longitude"""
+        lon = self.pos.get_apparent_lon().wrap_at(360*u.deg)
+        self.assertLess(np.abs(lon - c.Angle(199.90895*u.deg)), 1.0e-5*u.deg)
+        
+    def test_apparent_position(self) : 
+        """tests calculation of sun's apparent position"""
+        coord = self.pos.get_apparent_position()
+        self.assertLess(np.abs(coord.ra.wrap_at(180*u.deg) - c.Angle(-161.61917*u.deg)), 0.5e-5*u.deg)
+        self.assertLess(np.abs(coord.dec - c.Angle(-7.78507*u.deg)), 0.5e-5*u.deg)
+
+        
+class Ex15aVenus_pt1 (object) :         
+    """This class exists only to give the "Uptime" class venus ephemeris data
+    
+    Data comes from example 15 a pg. 103
+    """
+    def __init__(self, time) : 
+        self.disk_radius = 0 * u.deg
+        
+    def get_apparent_position(self) : 
+        ra = 41.73129 * u.deg
+        dec = 18.44092 * u.deg
+        return c.SkyCoord(ra=ra, dec=dec)
+        
+class Ex15aVenus_pt2 (Ex15aVenus_pt1) : 
+    """this class gives the uptime class the "interpolated" venus ephemeris
+    
+    Data comes from p. 104
+    """
+    def get_apparent_position(self): 
+        #  transit, rise, set
+        ra = [ 42.59324, 42.27648, 41.85927] * u.deg
+        dec = [0, 18.64229, 18.48835] * u.deg
+        return c.SkyCoord(ra=ra, dec=dec)
+        
+class TestUptime(unittest.TestCase) : 
+    def setUp(self) : 
+        self.t = t.Time("1988-03-20", scale='utc')
+        self.location = c.EarthLocation.from_geodetic(-71.0833, 42.3333)
+        self.up = sun.Uptime(Ex15aVenus_pt1, self.t, self.location)
+        # replace calculated sidereal time with given time
+        self.up.sidereal_greenwich = c.Angle( (11,50,58.10), unit=u.hour)
+        
+    @unittest.expectedFailure    
+    def test_sidereal(self) : 
+        """check that sidereal time calculation matches the "accurate ephemeris"
+        
+        p. 103
+        Note that this is marked as an "expected failure" because the astropy
+        method of calculating apparent sidereal time at greenwich does not agree
+        with the "given" value to the least significant figure given. It does, 
+        however, agree to within (6.5e-5 hourangle). (One least count is 2.7e-6 hourangle,
+        so this is approximately a factor of 20 more than the least count.
+        
+        This test failure is the reason we override the computed sidereal time in
+        the setup method.
+        """
+        sidereal = self.up.midnight_utc.sidereal_time("apparent","greenwich")
+        self.assertLess(np.abs(sidereal - self.up.sidereal_greenwich), c.Angle( (0,0,0.01),unit=u.hour))
+        
+    def test_delta_t(self) : 
+        """check the computed difference between ut and tt.
+        
+        From pg. 78: "...an instant given in UT is later than the instant
+        in TD having the same numerical value" (UT=universal time, TD=dynamical
+        time)
+        """
+        # self.t == UT on the UTC scale (should be later)
+        dt = self.t.datetime
+        
+        # tt == UTC numerical time on tt scale. (should be earlier)
+        tt = t.Time(dt, scale='tt')
+        delta_t = (self.t - tt) # later - earlier should be positive
+        
+        self.assertLess(np.abs(delta_t - 56*u.s), 0.5*u.s) 
+        
+    def test_approximate(self) :
+        """check calculation of approximate event times"""
+        approx = self.up.approximate()
+        
+        self.assertLess(np.abs(approx[0] - 0.81965 * u.sday), 1*u.s)
+        self.assertLess(np.abs(approx[1] - 0.51817 * u.sday), 1*u.s)
+        self.assertLess(np.abs(approx[2] - 0.12113 * u.sday), 1*u.s)
+        
+    def test_corrections(self) : 
+        """check calculation of corrections to approximate event times"""
+        # force calculation of approximate event times.
+        approx = self.up.approximate() 
+        
+        # change "body" object to object giving interpolated ephemeris
+        self.up.body_class = Ex15aVenus_pt2
+        cor = self.up.correction()
+        
+        self.assertLess(np.abs(cor[0] - 0.00015*u.day), 1*u.s)
+        self.assertLess(np.abs(cor[1] - (-0.00051 * u.day)), 1*u.s)
+        self.assertLess(np.abs(cor[2] - 0.00017 * u.day), 1*u.s)
+        
+    def test_approx_daylength(self) : 
+        """check calculation of daylength"""
+        approx_daylength = (0.12113 - 0.51817 + 1) * u.sday
+        
+        self.assertLess(np.abs(self.up.approximate_daylength() - approx_daylength), 1*u.s)
