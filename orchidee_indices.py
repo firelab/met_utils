added units tracking, fixed bug where I hardcoded the time axis.

orchidee_indices.py
@@ -37,7 +37,6 @@ class ForcingDataset ( agg.NetCDFTemplate ) :
         
     def get_xy_indices(self) : 
         if not hasattr(self, "_xy_indices") : 
-            forcing = self.get_forcing()
             ca = self.get_land_axes()
             self._xy_indices = ca.get_grid_indices()
         return self._xy_indices
@@ -78,28 +77,69 @@ class ForcingDataset ( agg.NetCDFTemplate ) :
             self._timestep = (times[1] - times[0])
         return self._timestep
         
-    def register_variable(self, varname) :
-        """adds "varname" to the list of variables to track""" 
+    def get_samples_per_day(self) : 
+        if not hasattr(self, '_spd') :
+            timestep = self.get_timestep()
+            self._spd = ((24*u.hour)/timestep).to(u.dimensionless_unscaled).astype(np.int)
+        return self._spd    
+        
+    def get_buffer_group(self) : 
         if not hasattr(self, "_buffers") : 
-            self._buffers = {} 
+            self._buffers = q.DLTSGroup() 
+        return self._buffers
+        
+    def get_derived_buffers(self) : 
+        if not hasattr(self, "_derived") : 
+            self._derived = q.DLTSGroup()
+            self._derived.template = self._buffers.template
+        return self._derived
+        
+    def register_variable(self, varname, unit) :
+        """adds "varname" to the list of variables to track""" 
+        bufs = self.get_buffer_group()
         
         forcing = self.get_forcing()
         ncvar = forcing.variables[varname]
-        time_axis = ncvar.dimensions.index('tstep')
-        self._buffers[varname] = q.DiurnalLocalTimeStatistics(
-            forcing.variables[varname], time_axis,
-            self.get_timestep(), self.get_longitudes())
-        return self._buffers[varname]
+        if not bufs.template_ready() : 
+            time_axis = ncvar.dimensions.index('tstep')
+            bufs.add(varname, q.DiurnalLocalTimeStatistics(
+                ncvar, time_axis,
+                self.get_timestep(), self.get_longitudes(), unit=unit))
+        else : 
+            bufs.create(varname, ncvar, unit)
+                
+        return bufs.get(varname)
+        
+    def compute_rh(self) : 
+        """Compute RH for the current utc day
+        
+        This calls "next()" on the internal RH object. No need to manage it."""
+        bufs = self.get_buffer_group()
+        derived = self.get_derived_buffers()
+        if "rh" in derived.group : 
+            # one day big
+            p = bufs.get('PSurf').get_utc_day()
+            q = bufs.get('Qair').get_utc_day()
+            t = bufs.get('Tair').get_utc_day()
+            rh = derived.get('rh')
+            rh.source = met.calc_rh_spec_humidity(q,p,t)
+            rh.next()
+        else :
+            # 2 days big
+            p = bufs.get('PSurf').get_buffer()
+            q = bufs.get('Qair').get_buffer()
+            t = bufs.get('Tair').get_buffer()
+            rh = derived.create_computed("rh",met.calc_rh_spec_humidity(q,p,t), 
+                    u.pct)
+            
+        return rh
+            
 
     def next(self) :
         """advance all of the registered variables to the next day"""
-        for key in self._buffers : 
-            self._buffers[key].next()
-        
-    def get_variable(self, varname) :
-        """returns the DiurnalLocalTimeStatistics object associated with the registered varname""" 
-        return self._buffers[varname]
-                
+        bufs = self.get_buffer_group()
+        bufs.next()
+                        
     def close(self) :
         """closes the input and output netCDF files""" 
         if hasattr(self, "_nc_forcing") : 
@@ -119,9 +159,9 @@ def indices_year(y, forcing_template, out_template) :
     ds = ForcingDataset(forcing_file, out_file)
     
     # register required variables so they are tracked
-    qair = ds.register_variable("Qair")
-    tair = ds.register_variable("Tair")
-    pres = ds.register_variable("Psurf")
+    qair = ds.register_variable("Qair", u.dimensionless_unscaled)
+    ds.register_variable("Tair", u.Kelvin)
+    ds.register_variable("PSurf", u.Pa)
 #    rainf = ds.register_variable("Rainf")
 #    swdown = ds.register_variable("SWdown")
     
@@ -148,6 +188,10 @@ def indices_year(y, forcing_template, out_template) :
     rh_min.title = "Minimum RH"
     rh_min.units = "percent"
     
+    rh_afternoon = ds.create_variable("rh_afternoon", ('days', 'land'),np.float32)
+    rh_afternoon.title = "RH in the afternoon"
+    rh_afternoon.units = "percent"
+        
     # base time
     time_start = t.Time('%04d:001'%y, format='yday', scale='ut1')
     if ( (y < 1960) or (y > 2013) ) :
@@ -166,7 +210,16 @@ def indices_year(y, forcing_template, out_template) :
             day.delta_ut1_utc = time_start.delta_ut1_utc
         daylength[i_day,:] = (ds.get_daylength_by_lat(day)).to(u.hour)
         
-        # calculate RH
+        # calculate RH & store
+        rh_dlts = ds.compute_rh()
+        rh_dlts.store_day(rh)
+        rh_max[i_day,:] = rh_dlts.max()
+        rh_min[i_day,:] = rh_dlts.min()
+        rh_afternoon[i_day,:] = rh_dlts.ref_val()
+        
+        # first time through, store the first day's data
+        if i_day == 1 : 
+            rh_dlts.store_day(rh, False)
         
         # load next day for all variables
         ds.next()

qty_index.py
@@ -4,6 +4,11 @@ import numpy.ma as ma
 import astropy.units as u
 import astropy.coordinates as c
 
+# cannot compare "unit" objects to python's None. 
+# creating an "unspecified" fundamental unit to signify that 
+# no unit object has been specified
+unspecified_u = u.def_unit("unspecified")
+
 class SamplingFunction (object) : 
     __metaclass__ = ABCMeta
     
@@ -70,7 +75,8 @@ class DiurnalLocalTimeStatistics (object) :
     required, the computation of statistics for "day 0" is not allowed. 
     """
     def __init__(self, source, time_axis=None, timestep=None, lons=None, 
-                  template=None, ref_time=13*u.hour, sequential=True):
+                  template=None, ref_time=13*u.hour, sequential=True, 
+                  unit=unspecified_u):
         """Wraps a data source to use as the basis of daily statistics.
         
         The caller must specify a numpy array-like data source, the index of the
@@ -92,6 +98,7 @@ class DiurnalLocalTimeStatistics (object) :
         self.i_buf_template = ( slice(None,None,None), ) * len(source.shape)
         self.buffer = None
         self.cur_day = None
+        self.unit = unit        
 
         # we either init most things off of a template, or we compute them 
         # from scratch
@@ -114,10 +121,10 @@ class DiurnalLocalTimeStatistics (object) :
         
         # if user wants sequential access, initialize buffer
         if sequential : 
-            self.__init_buffer()
+            self._init_buffer()
             
 
-    def __init_buffer(self) : 
+    def _init_buffer(self) : 
         self.load_day(1)
         self.cur_day = 2  
         
@@ -161,7 +168,7 @@ class DiurnalLocalTimeStatistics (object) :
         self.buffer_masked = ma.array(self.buffer, mask=self.mask)
 
         
-    def next(self, data=None) : 
+    def next(self) : 
         """loads the next day's data into the buffer
         
         Shifts the "current day's" data into the "yesterday" position in the
@@ -183,13 +190,9 @@ class DiurnalLocalTimeStatistics (object) :
         
         self.buffer[i_yesterday] = self.buffer[i_today]
         
-        if data == None : 
-            start = self.cur_day * self.diurnal_window
-            end   = start + self.diurnal_window
-            self.buffer[i_today] = self._get_src_data(start,end)
-        else : 
-            # if the user provided the next day's data, use it.
-            self.buffer[i_today] = data
+        start = self.cur_day * self.diurnal_window
+        end   = start + self.diurnal_window
+        self.buffer[i_today] = self._get_src_data(start,end)
         
         self.cur_day += 1
         
@@ -207,12 +210,19 @@ class DiurnalLocalTimeStatistics (object) :
         
         The time sample selected depends on the longitude of the cell. 
         """
+        # a vector the size of the compressed geospatial axis
         result = np.empty( (len(self.i_ref),), dtype=self.source.dtype )
         
         # 2nd day is the "current day".
         i_ref_buf = self.i_ref + self.diurnal_window
+        i_buf = [0,0]
+        i_time = self.time_axis
+        i_land = not i_time
         for i in range(len(i_ref_buf)) : 
-            result[i] = self.buffer[i, i_ref_buf[i]]
+            # buffer is 2D: timesteps x geospatial
+            i_buf[i_time] = i_ref_buf[i]
+            i_buf[i_land] = i
+            result[i] = self.buffer[i_buf]
         return result
     
     def get_preceeding_day(self) : 
@@ -240,9 +250,79 @@ class DiurnalLocalTimeStatistics (object) :
             j[i_time] = timeslice
             i_result[i_lon] = i
             result[i_result] = self.buffer[j]
-            
-        return result   
         
+        # set the units of the data if we know them    
+        if self.unit != unspecified_u : 
+            result = result * self.unit
+            
+        return result 
+        
+    def get_utc_day(self, current_day=True) : 
+        """Get the raw data for the current UTC day
+        
+        If you want the previous utc day's data, set current_day to False.
+        """
+        # construct the "today" index
+        start = 0 
+        if current_day : 
+            start = self.diurnal_window
+        day_slice = slice(start, start + self.diurnal_window)
+        i_day     = list(self.i_buf_template)
+        i_day[self.time_axis] = day_slice 
+
+        result = self.buffer[i_day]
+        
+        # set the units of the data if we know them
+        if self.unit != unspecified_u : 
+            result = result * self.unit
+            
+        return result
+        
+    def get_buffer(self) : 
+        """returns the buffer as a quantity object, if possible"""
+        result = self.buffer
+        if self.unit != unspecified_u : 
+            result = result * self.unit
+        return result
+        
+    def store_day(self, destination, current=True) : 
+        """stores one day of data from buffer to destination buffer
+        
+        destination is assumed to have same shape as source."""
+        day = self.cur_day - 1 # index of "today"
+        if not current : 
+            day -= 1 #index of yesterday
+        
+        start = day * self.diurnal_window
+        end = start + self.diurnal_window
+        timeslice = slice(int(start), int(end))
+        i_buf = list(self.i_buf_template)
+        i_buf[self.time_axis] = timeslice
+        
+        destination[i_buf] = self.get_utc_day(current)
+
+        
+
+class ComputedSourceDLTS (DiurnalLocalTimeStatistics) : 
+    """A diurnal local time statistics class where the source is computed
+    
+    This class draws new data from an ever changing source variable. Whereas the
+    parent class assumes the source value has a time axis which covers the
+    entire domain of the simulation (and therefore we need to load in one day 
+    at a time by advancing the index along the time axis), this class assumes 
+    the source contains exactly one day's data. The user is responsible for 
+    writing new values to the source before calling the next() method.
+    
+    You are explicitly allowed to assign new numpy arrays to the source attribute.
+    The source you initially provide should have two days of data, in order to 
+    initialize the internal 2-day buffer. The source buffer should be one day 
+    big by the time you call next().
+    """
+    def _get_src_data(self, start, end) : 
+        return self.source
+        
+           
+                        
 class DLTSGroup (object) : 
     """manages a group of related DiurnalLocalTimeStatistics objects
     
@@ -250,17 +330,20 @@ class DLTSGroup (object) :
     """
     def __init__(self) : 
         self.group = {} 
+        self.template = None
         
     def add(self, name, dlts) : 
+        """adds a named, pre-created DLTS object to the group."""
         if len(self.group)  == 0 : 
             self.template = dlts
         self.group[name] = dlts
         
     def next(self) : 
-        for k,v in self.group : 
-            v.next() 
+        """call next() method for each object in group"""
+        for k in self.group : 
+            self.group[k].next() 
             
-    def create(self, name, source) : 
+    def create(self, name, source, unit) : 
         """creates a new dlts object, using the template
         
         This method avoids the need to call the computationally expensive 
@@ -270,10 +353,22 @@ class DLTSGroup (object) :
         as a template. You must have called add() at least once prior to calling
         this function.
         """
-        self.group[name] = DiurnalLocalTimeStatistics(source,template=self.template)
+        self.group[name] = DiurnalLocalTimeStatistics(source,
+                template=self.template, unit=unit)
+        return self.group[name]
+        
+    def create_computed(self, name, source, unit) : 
+        """creates a computed dlts object using the template"""
+        self.group[name] = ComputedSourceDLTS(source, 
+            template=self.template, unit=unit)
         return self.group[name]
     
     def get(self, name) : 
+        """fetches a named dlts object"""
         return self.group[name]
+        
+    def template_ready(self) :
+        """are we ready to create objects using a template?""" 
+        return self.template != None
         
         

tests/test_qty_index.py
@@ -150,6 +150,24 @@ class TestDiurnalLocalTimeStatistics(unittest.TestCase) :
         
         self.assertTrue(np.all(result==x.ref_val()))
         
+        # now switch test data so that there's more land points than time points
+        # and the axes are reversed
+        self.test_data = np.arange(100*16).reshape(16,100)
+        self.time_axis = 0
+        self.lons = np.arange(100)*2*u.deg - 135*u.deg
+        x = q.DiurnalLocalTimeStatistics(self.test_data, self.time_axis, 
+                                          self.timestep, self.lons)
+        lsf = q.LongitudeSamplingFunction(24*u.hour/self.timestep, 13*u.hour)
+        i_lons = lsf.get_index(self.lons) + 4
+        
+        result = np.empty( (len(i_lons),) )
+        for i in range(len(i_lons)) : 
+            result[i] = self.test_data[i_lons[i],i]
+        
+        self.assertTrue(np.all(result==x.ref_val()))
+ 
+        
+        
     def test_preceeding_day(self): 
         """tests to ensure the correct data is returned"""
         x = q.DiurnalLocalTimeStatistics(self.test_data, self.time_axis, 
@@ -211,4 +229,19 @@ class TestDiurnalLocalTimeStatistics(unittest.TestCase) :
         self.assertTrue(np.all(x.mask == y.mask))
         self.assertTrue(np.all(x.source == self.test_data))
         self.assertTrue(np.all(y.source == v))
-
+        
+    def test_unit(self) : 
+        """make sure that returned data have correct units attached"""
+        x = q.DiurnalLocalTimeStatistics(self.test_data, self.time_axis, 
+                                          self.timestep, self.lons, unit=u.Pa)
+                                          
+        self.assertEqual(x.get_utc_day().unit, u.Pa)
+        self.assertEqual(x.get_preceeding_day().unit, u.Pa)
+        self.assertEqual(x.get_buffer().unit, u.Pa)
+        
+        # repeat for "no units" case
+        x = q.DiurnalLocalTimeStatistics(self.test_data, self.time_axis, 
+                                          self.timestep, self.lons)
+        self.assertFalse(hasattr(x.get_utc_day(), "unit"))
+        self.assertFalse(hasattr(x.get_preceeding_day(), "unit"))
+        self.assertFalse(hasattr(x.get_buffer(), "unit"))