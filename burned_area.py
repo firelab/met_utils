96066e7dd478d45d61c7b7583a6ada97081407f5
Nordgren <bnordgren@lugosi.usfs-i2.umt.edu>
2014-11-29 16:12:39 -0700

    bugfixes

--- burned_area.py
+++ burned_area.py
@@ -9,6 +9,7 @@ import numpy as np
 import astropy.units as u
 import astropy.time as time
 import aggregator as agg
+import dateutil.parser as dup
 from osgeo import ogr
 
 def ba_year(year, template, ncfile, shapefile)  :
@@ -22,31 +23,44 @@ def ba_year(year, template, ncfile, shapefile)  :
     ba.copyVariable('nav_lon')
     ba.copyDimension('days')
     srcvar = ba._ncfile.variables
+    dims   = ba._ncfile.dimensions
     
     # make indexer for various axes
-    lat_idx = q.LinearSamplingFunction(srcvar['nav_lat'][1,0]-srcvar['nav_lat'][0,0],
-                                        x_zero=srcvar['nav_lat'][0,0])
-    lon_idx = q.LinearSamplingFunction(srcvar['nav_lon'][0,1]-srcvar['nav_lon'][0,0],
-                                        x_zero=srcvar['nav_lon'][0,0])    
+    delta_lat=  (srcvar['nav_lat'][1,0]-srcvar['nav_lat'][0,0])*u.deg
+    delta_lon=  (srcvar['nav_lon'][0,1]-srcvar['nav_lon'][0,0])*u.deg
+    lat_idx = q.LinearSamplingFunction(1./delta_lat, x_zero=srcvar['nav_lat'][0,0]*u.deg)
+    lon_idx = q.LinearSamplingFunction(1./delta_lon, x_zero=srcvar['nav_lon'][0,0]*u.deg)    
     time_idx = q.TimeSinceEpochFunction(1*u.day, time.Time('%d:001'%year,format='yday'))
     idx = q.OrthoIndexer([lat_idx, lon_idx, time_idx])
     
     #create the netcdf variable
-    count = ba.create_variable('count', np.int, ('y','x','days'))
-    ui_count = q.UnitIndexNdArray(count, idx)
+    count = ba.create_variable('count', ('y','x','days'), np.int)
+    cache = np.zeros( (len(dims['y']),len(dims['x']), len(dims['days'])), dtype=np.int32)
+    ui_count = q.UnitIndexNdArray(cache, idx)
     
     # open the shapefile & get layer
     driver = ogr.GetDriverByName('ESRI Shapefile')
     dataSource = driver.Open(shapefile, 0) # 0 means read-only. 1 means writeable.
     layer = dataSource.GetLayer()
     
+    counter = 0
     for feature in layer : 
-        loc = feature.GetGeometryRef().GetPoint(0)
-        t = feature.GetField("DATE")
-        t_obj = time.Time(t)
+        if (counter % 100) == 0 : 
+            print counter
+        counter += 1
+        geom = feature.GetGeometryRef()
+        if geom.GetGeometryName() == 'POLYGON' : 
+            loc = geom.GetGeometryRef(0).GetPoint(0) * u.deg
+            t = feature.GetField("DATE")
+            dt = dup.parse(t)
+            # convert to UTC if necessary
+            if dt.tzinfo is not None: 
+                dt = (dt - dt.utcoffset()).replace(tzinfo=None)
+            t_obj = time.Time(dt)
         
-        # increment the correct counter
-        ui_count.inc( (loc[1],loc[0],t_obj))
+            # increment the correct counter
+            ui_count.inc( (loc[1],loc[0],t_obj))
         
+    count[:] = cache[:]
     ba.close()
     
